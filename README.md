33cce3d62e1dd2f1cc0cab76e0101fd88399e4cd
Rajat Tandon <rajattan@usc.edu>
2021-03-14 19:23:35 -0700

    SCRIPT for No. of Transactions DATEWISE

--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 #========================================#
 Scripts for the Venmo project.
 
+A. GET NUMBER OF TRANSACTIONS DATEWISE.
+#====================================================================================================#
+# python3 datewise_transactions_count.py <path to the input json file>  <path to the output file>    #
+# Example:                                                                                           #
+#python3 datewise_transactions_count.py /Users/rajattan/venmo/dummy.json ./transactions_date_wise.txt#
+#====================================================================================================#
+
 
---
+++ datewise_transactions_count.py
@@ -0,0 +1,50 @@
+#====================================================================================================#
+#*******************************   HOW TO RUN?  **************************************************** #
+# python3 datewise_transactions_count.py <path to the input json file>  <path to the output file>    #
+#*************************************************************************************************** #
+# Example:                                                                                           #
+#python3 datewise_transactions_count.py /Users/rajattan/venmo/dummy.json ./transactions_date_wise.txt#
+#====================================================================================================#
+
+import sys
+import json
+
+transactions = 0
+transactions_date_wise = {}
+
+
+if(len(sys.argv) != 3):
+    print("==========================================================================")
+    print("SORRY!! Please provide the path to the INPUT json file and the OUTPUT file")
+    print("==========================================================================")
+    print("Example: python3 datewise_transactions_count.py ./dummy.json ./output.txt ")
+    print("==========================================================================")
+    sys.exit()
+
+
+
+f = open(sys.argv[1])
+outputfile = open(sys.argv[2],"w")
+
+for line in f:
+    data = json.loads(line)
+    transactions = transactions + 1
+    try:
+        if(data is None or data['created_time'] is None):
+            continue
+        datetime = str(data['created_time'])
+        date = datetime.split("T")
+        if(date[0] not in transactions_date_wise):
+            transactions_date_wise[date[0]] = 0
+        transactions_date_wise[date[0]] = transactions_date_wise[date[0]] + 1
+        #print(datetime)
+    except TypeError:
+        continue        
+f.close()
+
+outputfile.write("DATE #TRANSACTIONS \n")
+for k,v in transactions_date_wise.items():
+    outputfile.write(str(k) + " " + str(v) + "\n")
+
+outputfile.write("TOTAL NUMBER OF TRANSACTIONS ARE :" + str(transactions))
+outputfile.close()
