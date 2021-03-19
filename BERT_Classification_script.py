746b1deab8d05c6d454a5614903be525c2eff261
Rajat Tandon <rajattan@usc.edu>
2021-03-19 10:49:54 -0700

    Added BertClassifier class

--- BERT_Classification_script.py
+++ BERT_Classification_script.py
@@ -17,12 +17,32 @@ from transformers import TFBertModel
 from transformers import BertTokenizer
 from tensorflow.keras.layers import Dense, Flatten
 
+#===============================================================#
 transactions = 0
 textual_transactions = 0
 transactions_date_wise = {}
 pattern = re.compile("[A-Za-z0-9]+")
 phno = re.compile("\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}")
 email = re.compile("[^@]+@[^@]+\.[^@]+")
+
+#===============================================================#
+class BertClassifier(tf.keras.Model):    
+    def __init__(self, bert: TFBertModel, num_classes: int):
+        super().__init__()
+        self.bert = bert
+        self.classifier = Dense(num_classes, activation='sigmoid')
+        
+    @tf.function
+    def call(self, input_ids, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None):
+        outputs = self.bert(input_ids,
+                               attention_mask=attention_mask,
+                               token_type_ids=token_type_ids,
+                               position_ids=position_ids,
+                               head_mask=head_mask)
+        cls_output = outputs[1]
+        cls_output = self.classifier(cls_output)
+                
+        return cls_output
 
 #===============================================================#
 """
