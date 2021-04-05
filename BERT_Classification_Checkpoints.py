c6cd004fa83f8bbede664e492a3e003fce8f21aa
Rajat Tandon <rajattan@usc.edu>
2021-04-04 22:26:45 -0700

    uncommented a line

--- BERT_Classification_Checkpoints.py
+++ BERT_Classification_Checkpoints.py
@@ -259,7 +259,7 @@ for line in f:
         if(data is None or data['message'] is None or data['message'] == ""):
             continue
         note = str(data['message'])
-        tokens = nltk.word_tokenize(note)
+        #tokens = nltk.word_tokenize(note)
         tokens = preprocessing(note)
 
         if('actor' not in data or 'username' not in data['actor'] or 'transactions' not in data or data['transactions'] is None  or 'target' not in data['transactions'][0] or 'username' not in data['transactions'][0]['target']):
