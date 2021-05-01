955fd7802f949a9895ea2c8fbc8cf39f9130ade4
Rajat Tandon <rajattan@usc.edu>
2021-05-01 08:33:48 -0700

    Minor changes

--- merge_updated_format.py
+++ merge_updated_format.py
@@ -43,7 +43,6 @@ def updateStat(username, joined, sender, receiver):
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         print(exc_type, fname, exc_tb.tb_lineno)
-        continue
 
 
 #===============================================================#
