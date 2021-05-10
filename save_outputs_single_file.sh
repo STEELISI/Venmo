7407ebf25447d9ab7c61df82e034f187982ce4fb
Rajat Tandon <rajattan@usc.edu>
2021-05-10 13:54:14 -0700

    save_outputs_single_file.sh

---
+++ save_outputs_single_file.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#==================================================================================#
+# bash helper.sh                                                                   #
+#==================================================================================#
+CHECKPOINTFOLDER="checkpoint_MAY_1_2020/"
+OUTPUT_FILES_PATH="tmp/out"
+RANGE=1
+OFFSET=1
+
+#Make tmp dir
+mkdir -p $TEMPORARY_FOLDER_FOR_INTERMEDIATE_MERGES
+
+#Count number of files in CHECKPOINTFOLDER
+files=$(find $CHECKPOINTFOLDER  -maxdepth 1 -type f| grep sender.txt | awk -F 'sender.txt.' '{print $2}')
+count=$(find $CHECKPOINTFOLDER  -maxdepth 1 -type f| grep sender.txt | wc -l)
+echo $count
+echo $files
+
+
+#i=$RANGE
+#while [ $i -le $count ] ; do
+#  #echo "Processing $i to $(($i+$OFFSET-1))"
+#  #python3 merge_helper.py $CHECKPOINTFOLDER $TEMPORARY_FOLDER_FOR_INTERMEDIATE_MERGES $i $OFFSET
+#  i=$(($i+$OFFSET))
+#done
+
+for word in $files
+do
+    python3 merge_w_hashing.py $CHECKPOINTFOLDER  $OUTPUT_FILES_PATH $word
+    echo "\n Processing of sender.txt.$word and reciever.txt.$word complete! \n"
+done
