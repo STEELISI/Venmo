536cfde59507d5929c4133f4adcd78b8c0fad2a9
Rajat Tandon <rajattan@usc.edu>
2021-05-19 21:49:28 -0700

    pull_more_Group_details.py

--- pull_more_Group_details.py
+++ pull_more_Group_details.py
@@ -21,9 +21,9 @@ USERS_FILE = "checkpoint_part1/PARTIAL_OUTPUT.txt"
 
 if(len(sys.argv) != 1):
     print("==========================================================================")
-    print("SORRY!! Please provide the path to the INPUT json file and the OUTPUT file")
+    print("SORRY!! Please provide the  OUTPUT file")
     print("==========================================================================")
-    print("Example: python3 BERT_Classification_script.py ./dummy.json ./output.txt  ")
+    print("Example: python3 pull_more_Group_details.py  ./output.txt  ")
     print("==========================================================================")
     sys.exit()
 
